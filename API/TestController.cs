1c8721089676bda9c5525d1840c2e5ee5fe3d533
renaldy.widjaja39 <renaldy.widjaja@titansoft.com.sg>
2019-06-08 15:03:16 +0700

    first commit

---
+++ API/TestController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetcore.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("Connection is okay");
+        }
+
+        [HttpPost("getbalance2/{id}")]
+        public ActionResult GetBalance(int id)
+        {
+            var balance = new PlayerBalance { DiamondBalance = 100, StarBalance = 1000 };
+            return Ok(balance);
+        }
+
+
+        [HttpPost("getbalance")]
+        public ActionResult GetBalance(GetBalanceRequest request)
+        {
+            var balance = new PlayerBalance { DiamondBalance = 100, StarBalance = 1000 };
+            return Ok(balance);
+        }
+
+        [Produces("application/json")]
+        [HttpPost("getuserrankings")]
+        public ActionResult GetUserRankings()
+        {
+            var userRankings = new List<UserRanking>()
+            {
+                new UserRanking {Username = "TestPlayer1", Ranking = 1},
+                new UserRanking {Username = "TestPlayer2", Ranking = 2},
+                new UserRanking {Username = "TestPlayer3", Ranking = 3}
+            };
+            return Ok(userRankings);
+        }
+    }
+    public class UserRanking
+    {
+        public string Username { get; set; }
+        public int Ranking { get; set; }
+    }
+
+    public class PlayerBalance
+    {
+        public int StarBalance { get; set; }
+        public int DiamondBalance { get; set; }
+    }
+
+    public class GetBalanceRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace aspnetcore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(x => { x.SwaggerDoc("v1", new Info
+            {
+                Title = "My Api",
+                Version = "v1"
+            }); });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(x =>
+            {
+                x.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseCors("Cors Policy");
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
